8a90787a9785eeb3faa5607f832fdc3784fc4359
Jonathan Bridge <jvbridge@gmail.com>
2022-04-20 12:18:18 -0700

    a few more questions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const defaultJson = require("defaults.json"); // default values to use for HTML generation
+
 /**
  * an array of questions to be sent to inquirer
  * @type {object[]} - each questoin is its own object
@@ -7,10 +9,31 @@ const inquirer = require("inquirer");
 const questions = [
     {
         name: "userName",
-        message: "tell me your name"
+        message: "tell me your name",
+        type:"input"
+    },
+    {
+        name: "location",
+        message: "Where are you located?",
+        type:"input"
+    },
+    {
+        name: "bio",
+        message: "Tell me a little about yourself",
+        type:"input"
+    },
+    {
+        name: "linkedInURL",
+        message: "What is your linked in?",
+        type:"input"
+    },
+    {
+        name: "githubURL",
+        message: "What is your github?",
+        type:"input"
     }
 ];
 
 inquirer.prompt(questions).then((answers) =>{
-    console.log("userName:", answers.userName);
+    console.log("answer", answers);
 });
