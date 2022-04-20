8ec5c355130c05f8e4a4f38c6e9ca624d737a1d0
Jake Walsh <jkwalsh127@gmail.com>
2022-04-20 13:08:27 -0700

    add styles to generateHTML string literal

--- util/generator.js
+++ util/generator.js
@@ -1,7 +1,7 @@
 
 function generateHTML(data){
     const gitHubLink = `https://github.com/${data.gitHubURL}`
-    return layout `<!DOCTYPE html>
+    return `<!DOCTYPE html>
     <html lang="en-US">
     <head>
         <meta charset="UTF-8">
@@ -9,6 +9,88 @@ function generateHTML(data){
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./assets/css/style.css">
         <title>Portfolio</title>
+        <style>
+        #name {
+          width: 100%;
+          overflow: overlay;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+        }
+        #location {
+          width: 100%;
+          overflow: overlay;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+        }
+        .content{
+          width: 100%;
+          overflow: overlay;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+        }
+        .content article {
+          background-color: white;
+          width: 50%;
+        }
+        .content-header {
+          margin: 100px 0 20px 5%;
+          font-size: 2em;
+          width: 100%;
+        }
+        .content p {
+          line-height: 130%;
+          font-size: 1.2em;
+          text-indent: 50px;
+      
+        }
+        #contact-me {
+          overflow: overlay;
+          width: 100%;
+          background-color: white;
+          display: flex;
+          justify-content: space-between;
+          align-content: center;
+          flex-direction: column;
+        }
+        #contact-me-header {
+          width: 95%;
+          margin: 100px 0 20px 5%;
+          font-size: 2em;
+        }
+        .hr-full {
+          width: 95%;
+          float: left;
+          margin: 0 0 5px 0;
+        }
+        #contact-ul {
+          color: black;
+          font-size: 100%;
+          display: flex;
+          justify-content: space-around;
+          align-content: center;
+          margin-right: 10px;
+          font-weight: bold;
+          transition: 0.4s;
+          height: 100px;
+          width: 80%;
+          margin: 0px 10%;
+          list-style: none;
+        }
+        #contact-ul li {
+          display: flex;
+          align-self: center;
+        }
+        .contact-link {
+          text-decoration: underline;
+        }
+        .social-logo {
+          width: 16px;
+          margin-right: 15px;
+        }
+        </style>
     </head>
     <h1 id="name">${data.userName}</h1>
     <h2 id="location">${data.location}</h2>
