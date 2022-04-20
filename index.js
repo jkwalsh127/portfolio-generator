b906c9902257964445c4091ce5a2cd9a7f1276d2
Jonathan Bridge <jvbridge@gmail.com>
2022-04-20 12:11:48 -0700

    added first dummy question

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+/**
+ * an array of questions to be sent to inquirer
+ * @type {object[]} - each questoin is its own object
+ */
+const questions = [
+    {
+        name: "userName",
+        message: "tell me your name"
+    }
+];
 
-
-
+inquirer.prompt(questions).then((answers) =>{
+    console.log("userName:", answers.userName);
+});
